ef91e064ff2ad95b9d2c2c0b2594f4f6e0dbf053
Ravi9140 <78776196+Ravi9140@users.noreply.github.com>
2022-01-25 18:38:40 +0530

    Update digitOccurence.c

--- digitOccurence.c
+++ digitOccurence.c
@@ -1,10 +1,10 @@
 #include<stdio.h>
 #include<stdlib.h>
 
-void occurence(unsigned int long long num)
+void occurence(unsigned int num)
 {
 	int i,div,arr[10];
-	unsigned int long long rem;
+	unsigned int rem;
 	
 	//Assigning value to array elements as they hold Garbage values by default
 	for(i=0;i<10;i++)
@@ -12,54 +12,19 @@ void occurence(unsigned int long long num)
 	
 	while(num>0)
 	{
-		//arr[(num % 10)]++;  Another method
-		rem=num%10;
+		arr[(num % 10)]++;  
 		num/=10;
-		
-		//Increment digit occurence for each appearance 
-		switch(rem)
-		{
-			case 0: arr[0]++;
-					break;
-			
-			case 1: arr[1]++;
-					break;
-					
-			case 2: arr[2]++;
-					break;
-					
-			case 3: arr[3]++;
-					break;
-					
-			case 4: arr[4]++;
-					break;
-					
-			case 5: arr[5]++;
-					break;
-					
-			case 6: arr[6]++;
-					break;
-					
-			case 7: arr[7]++;
-					break;
-					
-			case 8: arr[8]++;
-					break;
-					
-			case 9: arr[9]++;
-					break;
-		}
 	}
 	printf("\nOccurences of Each Digits in the Given Number are:\n");
 	for(i=0;i<10;i++)
 	{
 		printf("\n%d = %d Occurences",i,arr[i]);
-    }
+  	}
 }
 
 int main()
 {
-	unsigned int long long number;
+	unsigned int number;
 	
 	do
 	{
@@ -67,7 +32,7 @@ int main()
 		scanf("%d",&number);
 		if(number<1)
 			printf("\nNumber should be greater than 0!!!\n");
-    }while(number<1);
+  	}while(number<1);
     
-    occurence(number);
+  	occurence(number);
 }
